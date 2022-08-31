6763c1a12678acd2bd21b14ddee9d96b3663f49b
Sunho-C <shchoi@yookgak.com>
2022-08-31 15:52:44 +0900

    gallery update

--- js/gallery.js
+++ js/gallery.js
@@ -1,9 +1,26 @@
 (function(){
     const imgWrap = document.querySelector('.img-wrap');
-    for(let i=0; i<10; i++){
-    let div = document.createElement('div');
-    div.innerHTML = i;
-    div.setAttribute('style', `--row:${Math.floor(Math.random() * 3 + 1)}; --col:${Math.floor(Math.random() * 2 + 1)};`);
-    imgWrap.appendChild(div);
+    const imgPop = document.querySelector('.img-pop');
+    let html='';
+    // --row:${Math.floor(Math.random() * 2 + 1)}; --col:${Math.floor(Math.random() * 2 + 1)};
+    for(let i=0; i<19; i++){
+        html += `<li style="background-image: url(./images/img_${i}.jpg);" data-acc="${i}">
+                </li>`
     };
+    imgWrap.insertAdjacentHTML("beforeend", html);
+
+    imgWrap.addEventListener('click', (e) => {
+        const el = e.target;
+        if (el.tagName === 'LI') {
+            imgWrap.classList.add('show');
+            imgPop.children[0].src = `./images/img_${el.dataset.acc}.jpg`;
+        }
+    });
+
+    imgPop.addEventListener('click', (e) => {
+        const el = e.target;
+        if (el.tagName === 'DIV') {
+            imgWrap.classList.remove('show')
+        }
+    });
 })()
