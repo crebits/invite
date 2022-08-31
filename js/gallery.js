13ff0752ccae87fef5202b9a7a652307966e457e
Sunho-C <shchoi@yookgak.com>
2022-08-31 10:42:01 +0900

    gallery & css 변경

--- js/gallery.js
+++ js/gallery.js
@@ -3,7 +3,7 @@
     for(let i=0; i<10; i++){
     let div = document.createElement('div');
     div.innerHTML = i;
-    div.setAttribute('style', `--row:${Math.floor(Math.random() * 2 + 1)}; --col:${Math.floor(Math.random() * 2 + 1)};`);
+    div.setAttribute('style', `--row:${Math.floor(Math.random() * 3 + 1)}; --col:${Math.floor(Math.random() * 2 + 1)};`);
     imgWrap.appendChild(div);
     };
 })()
