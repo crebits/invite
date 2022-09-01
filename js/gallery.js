80d89a76331437c26132ce4e9fcda46c339948bd
Sunho-C <shchoi@yookgak.com>
2022-09-01 13:52:12 +0900

    버튼 / 갤러리

--- js/gallery.js
+++ js/gallery.js
@@ -1,19 +1,27 @@
 (function(){
     const imgWrap = document.querySelector('.img-wrap');
     const imgPop = document.querySelector('.img-pop');
+    const weddingImg = document.querySelector('.wedding');
     let html='';
     // --row:${Math.floor(Math.random() * 2 + 1)}; --col:${Math.floor(Math.random() * 2 + 1)};
-    for(let i=0; i<19; i++){
+    for(let i=0; i<16; i++){
         html += `<li style="background-image: url(./images/img_${i}.jpg);" data-acc="${i}">
                 </li>`
     };
     imgWrap.insertAdjacentHTML("beforeend", html);
 
+    const imageNmae = (count) => {
+        if(count < 0) count = 15
+        if(count > 15) count = 0
+        weddingImg.src = `./images/img_${count}.jpg`;
+        weddingImg.dataset.acc = count;
+    }
+
     imgWrap.addEventListener('click', (e) => {
         const el = e.target;
         if (el.tagName === 'LI') {
             imgWrap.classList.add('show');
-            imgPop.children[0].src = `./images/img_${el.dataset.acc}.jpg`;
+            imageNmae(el.dataset.acc)
         }
     });
 
@@ -21,6 +29,10 @@
         const el = e.target;
         if (el.tagName === 'DIV') {
             imgWrap.classList.remove('show')
+        }
+        if (el.tagName === 'BUTTON') {
+            el.className ==='prev' ? weddingImg.dataset.acc-- : weddingImg.dataset.acc++;
+            imageNmae(weddingImg.dataset.acc)
         }
     });
 })()
