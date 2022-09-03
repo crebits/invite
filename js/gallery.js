310c8914b56caee6af8152325a21d0d8ad1b0ddd
Sunho-C <shchoi@yookgak.com>
2022-09-03 11:24:33 +0900

    이미지 수정

--- js/gallery.js
+++ js/gallery.js
@@ -4,15 +4,15 @@
     const weddingImg = document.querySelector('.wedding');
     let html='';
     // --row:${Math.floor(Math.random() * 2 + 1)}; --col:${Math.floor(Math.random() * 2 + 1)};
-    for(let i=0; i<16; i++){
+    for(let i=0; i<18; i++){
         html += `<li style="background-image: url(./images/img_${i}.jpg);" data-acc="${i}">
                 </li>`
     };
     imgWrap.insertAdjacentHTML("beforeend", html);
 
     const imageNmae = (count) => {
-        if(count < 0) count = 15
-        if(count > 15) count = 0
+        if(count < 0) count = 18
+        if(count > 18) count = 0
         weddingImg.src = `./images/img_${count}.jpg`;
         weddingImg.dataset.acc = count;
     }
